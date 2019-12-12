d7acb5a2f51b921e40de2ebbec21f4764190721b
Ronald Villaver <r.villaver@gmail.com>
2019-12-12 23:20:09 +0800

    init commit

---
+++ ios/Pods/Target Support Files/Pods-pulseplay-tvOSTests/Pods-pulseplay-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pulseplay_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_pulseplay_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-pulseplayTests/Pods-pulseplayTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pulseplayTests : NSObject
+@end
+@implementation PodsDummy_Pods_pulseplayTests
+@end
